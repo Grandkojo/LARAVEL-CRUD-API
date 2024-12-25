ee07a5794f7f18c4c38990b86fcbf13cae73499e
Grandkojo <essienernest.kojoowusu@gmail.com>
2024-12-25 08:51:01 +0000

    changed key from todos to data

--- app/Http/Controllers/API/TodoController.php
+++ app/Http/Controllers/API/TodoController.php
@@ -55,7 +55,7 @@ class TodoController extends Controller
 
             $data = [
                 'status_code' => '200',
-                'todos' => $todos
+                'data' => $todos
             ];
 
             return response()->json($data, 200);
